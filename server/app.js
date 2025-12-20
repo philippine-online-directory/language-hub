ac1f35cb6fedc35760ac0b457c4035948dcff455
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-12-20 16:34:00 +0800

    Server can now run through app.js

--- server/app.js
+++ server/app.js
@@ -21,3 +21,8 @@ app.use('/', authRouter)
 app.use('/contributions', contributionRouter)
 app.use('/languages', languageRouter)
 app.use('/sets', setRouter)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
