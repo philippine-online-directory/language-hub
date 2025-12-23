dede1ef78a7a42ca582b7b26eab5e2482143dbf8
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-12-23 11:45:09 +0800

    Configured an axios instance and added intercepters to handle requests, responses, and errors

---
+++ client/hub-client/src/api/axiosConfig.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+api.interceptors.request.use(
+    //Reads and attatches authToken from local storage
+    (config) => {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+api.interceptors.response.use(
+    //Removes auth token and redirects to login page
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('authToken');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default api;
