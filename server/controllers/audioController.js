f317bdb8569948f1956f1903a2d27d6c3828441f
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2026-01-31 17:48:03 +0800

    Added controller for getting an upload url

---
+++ server/controllers/audioController.js
@@ -0,0 +1,43 @@
+import auth from '../middleware/auth.js'
+import s3Service from '../services/s3Service.js'
+import { body, matchedData } from 'express-validator'
+import validationErrorCheck from '../middleware/expressValidate.js'
+
+const validateUploadRequest = [
+    body('fileName').notEmpty()
+        .withMessage('fileName is required')
+        .trim(),
+    body('contentType').notEmpty()
+        .withMessage('contentType is required')
+        .isIn(['audio/mpeg', 'audio/mp3', 'audio/wav', 'audio/ogg', 'audio/webm', 'audio/m4a'])
+        .withMessage('Invalid audio content type')
+]
+
+const getUploadUrl = [
+    auth,
+    validateUploadRequest,
+    validationErrorCheck,
+    async (req, res, next) => {
+        try {
+            const { fileName, contentType } = matchedData(req);
+
+            const s3Key = s3Service.generateS3Key(fileName);
+
+            const uploadUrl = await s3Service.generateUploadUrl(s3Key, contentType);
+
+            res.status(200).json({
+                uploadUrl,
+                s3Key, 
+            });
+        } 
+        catch (err) {
+            next(err)
+        }
+    }
+]
+
+const audioController = {
+    getUploadUrl
+}
+
+export default audioController
